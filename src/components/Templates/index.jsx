3db0ff5df8abb0a04c894da8b739d11346360e03
nogi <Createc369.Re@gmail.com>
2023-10-13 19:10:17 +0900

    add: CreateUI

--- src/components/Templates/index.jsx
+++ src/components/Templates/index.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import { INIT_TODO_LIST } from "../../constants/data";
 import { TodoList } from "../Organisms/TodoList";
 import styles from "./styles.module.css";
+import { AddTodo } from "../Organisms/AddTodo";
 
 export const TodoTemplate = () => {
   const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
+  const [inputValue, setInputValue] = useState("");
+
+  const onChangeAddInputValue = () => {};
+
+  const hendleAddTodo = () => {};
 
   const handleDeleteTodo = (targetId, targetTitle) => {
     if (window.confirm(`「${targetTitle}」のtodoを削除しますか？`)) {
@@ -17,6 +23,13 @@ export const TodoTemplate = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Todo List</h1>
+      <section className={styles.common}>
+        <AddTodo
+          addInputValue={inputValue}
+          handleAddTodo={hendleAddTodo}
+          onChnageTodo={onChangeAddInputValue}
+        />
+      </section>
       <section className={styles.common}>
         <TodoList
           todoList={originTodoList}
