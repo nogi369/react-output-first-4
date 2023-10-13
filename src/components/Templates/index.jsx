326cd5a4453afd44d20cf002f1b98e57d087cbe2
nogi <Createc369.Re@gmail.com>
2023-10-13 20:18:45 +0900

    update: rename

--- src/components/Templates/index.jsx
+++ src/components/Templates/index.jsx
@@ -6,27 +6,27 @@ import { AddTodo } from "../Organisms/AddTodo";
 
 export const TodoTemplate = () => {
   const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
-  const [inputValue, setInputValue] = useState("");
+  const [addInputValue, setAddInputValue] = useState("");
   const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
 
-  const onChangeAddInputValue = (e) => setInputValue(e.target.value);
+  const onChangeAddInputValue = (e) => setAddInputValue(e.target.value);
 
   const hendleAddTodo = (e) => {
     // 入力フォームが空文字でない かつ Enterキーが押された場合
-    if (inputValue !== "" && e.key === "Enter") {
+    if (addInputValue !== "" && e.key === "Enter") {
       const nextUniqueId = uniqueId + 1;
 
       // Todoを新規登録する
-      const newTodo = [
+      const newTodoList = [
         ...originTodoList,
         {
           id: nextUniqueId,
-          title: inputValue,
+          title: addInputValue,
         },
       ];
 
       setUniqueId(nextUniqueId);
-      setOriginTodoList(newTodo);
+      setOriginTodoList(newTodoList);
     }
   };
 
@@ -43,7 +43,7 @@ export const TodoTemplate = () => {
       <h1 className={styles.title}>Todo List</h1>
       <section className={styles.common}>
         <AddTodo
-          addInputValue={inputValue}
+          addInputValue={addInputValue}
           handleAddTodo={hendleAddTodo}
           onChnageTodo={onChangeAddInputValue}
         />
