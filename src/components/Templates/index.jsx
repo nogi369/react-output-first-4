2f9661e0b6394ee2cd7629c6df8787a84c09484a
nogi <Createc369.Re@gmail.com>
2023-10-13 11:56:36 +0900

    add: Delete1

--- src/components/Templates/index.jsx
+++ src/components/Templates/index.jsx
@@ -6,11 +6,20 @@ import styles from "./styles.module.css";
 export const TodoTemplate = () => {
   const [originTodoList, setOriginTodoList] = useState(INIT_TODO_LIST);
 
+  const handleDeleteTodo = (id) => {
+    const newTodos = originTodoList.filter((todo) => todo.id !== id);
+
+    setOriginTodoList(newTodos);
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Todo List</h1>
       <section className={styles.common}>
-        <TodoList todoList={originTodoList} />
+        <TodoList
+          todoList={originTodoList}
+          handleDeleteTodo={handleDeleteTodo}
+        />
       </section>
     </div>
   );
